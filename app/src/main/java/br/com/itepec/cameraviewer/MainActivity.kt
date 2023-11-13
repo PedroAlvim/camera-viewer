93030943446fcdf58179945db3500b4fe0bb4fe2
Pedro Alvim <pedro@sentry.com.br>
2023-11-13 15:38:18 -0300

    design update and zoom implementation

--- app/src/main/java/br/com/itepec/cameraviewer/MainActivity.kt
+++ app/src/main/java/br/com/itepec/cameraviewer/MainActivity.kt
@@ -2,12 +2,12 @@ package br.com.itepec.cameraviewer
 
 import android.Manifest
 import android.content.pm.PackageManager
+import android.content.res.ColorStateList
 import android.os.Bundle
 import android.util.Log
-import android.view.View
-import android.widget.CheckBox
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.camera.core.Camera
 import androidx.camera.core.CameraSelector
 import androidx.camera.core.ImageCapture
 import androidx.camera.core.Preview
@@ -18,22 +18,32 @@ import androidx.core.content.ContextCompat
 import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.isVisible
+import br.com.itepec.cameraviewer.databinding.ActivityMainBinding
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     private var imageCapture: ImageCapture? = null
     private lateinit var cameraExecutor: ExecutorService
+    private var camera: Camera? = null
+
+    private var gridActivated = false
+
+    private var currentZoomRatio = 1f
+    private val zoomStep = 0.5f
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val windowInsetsController =
-            WindowCompat.getInsetsController(window, window.decorView)
+        val windowInsetsController = WindowCompat.getInsetsController(window, window.decorView)
 
         windowInsetsController.hide(WindowInsetsCompat.Type.systemBars())
 
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
 
         supportActionBar?.hide()
 
@@ -44,14 +54,67 @@ class MainActivity : AppCompatActivity() {
         }
 
         setGridBehavior()
+        setZoomBehavior()
 
         cameraExecutor = Executors.newSingleThreadExecutor()
     }
 
     private fun setGridBehavior() {
-        findViewById<CheckBox>(R.id.grid_check_box).setOnCheckedChangeListener { _, isChecked ->
-            findViewById<View>(R.id.grid1).isVisible = isChecked
-            findViewById<View>(R.id.grid2).isVisible = isChecked
+        binding.gridBtn.setOnClickListener {
+            if (gridActivated) {
+                binding.grid1.isVisible = false
+                binding.grid2.isVisible = false
+
+                binding.gridBtn.setImageResource(R.drawable.ic_grid)
+            } else {
+                binding.grid1.isVisible = true
+                binding.grid2.isVisible = true
+
+                binding.gridBtn.setImageResource(R.drawable.ic_grid_off)
+            }
+            gridActivated = !gridActivated
+        }
+    }
+
+    private fun setZoomBehavior() {
+        binding.btn3.setOnClickListener {
+            currentZoomRatio += zoomStep
+
+            val maxZoomRatio = camera?.cameraInfo?.zoomState?.value?.maxZoomRatio ?: 1f
+            if (currentZoomRatio > maxZoomRatio) {
+                currentZoomRatio = maxZoomRatio
+            }
+
+            camera?.cameraControl?.setZoomRatio(currentZoomRatio)
+            updateZoomButtonsState()
+        }
+
+        binding.btn1.setOnClickListener {
+            currentZoomRatio -= zoomStep
+
+            // Verifique se o nível de zoom está abaixo do zoom mínimo
+            val minZoomRatio = camera?.cameraInfo?.zoomState?.value?.minZoomRatio ?: 1f
+            if (currentZoomRatio < minZoomRatio) {
+                currentZoomRatio = minZoomRatio
+            }
+
+            camera?.cameraControl?.setZoomRatio(currentZoomRatio)
+            updateZoomButtonsState()
+        }
+
+        updateZoomButtonsState()
+    }
+
+    private fun updateZoomButtonsState() {
+        val maxZoomRatio = camera?.cameraInfo?.zoomState?.value?.maxZoomRatio ?: 1f
+        val minZoomRatio = camera?.cameraInfo?.zoomState?.value?.minZoomRatio ?: 1f
+
+        binding.btn3.isEnabled = currentZoomRatio < maxZoomRatio
+
+        binding.btn1.isEnabled = currentZoomRatio > minZoomRatio
+
+        if (!binding.btn3.isEnabled && !binding.btn1.isEnabled) {
+            binding.btn3.isEnabled = true
         }
     }
 
@@ -59,31 +122,29 @@ class MainActivity : AppCompatActivity() {
         val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
 
         cameraProviderFuture.addListener({
-            // Used to bind the lifecycle of cameras to the lifecycle owner
+            // Usado para vincular o ciclo de vida das câmeras ao proprietário do ciclo de vida
             val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
 
-            val preview = Preview.Builder()
-                .build()
-                .also {
-                    it.setSurfaceProvider(findViewById<PreviewView>(R.id.viewFinder).createSurfaceProvider())
-                }
+            val preview = Preview.Builder().build().also {
+                it.setSurfaceProvider(findViewById<PreviewView>(R.id.viewFinder).createSurfaceProvider())
+            }
 
             imageCapture = ImageCapture.Builder().build()
 
-            // Select back camera as a default
+            // Seleciona a câmera traseira como padrão
             val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
 
             try {
-                // Unbind use cases before rebinding
+                // Desvincule os casos de uso antes de vincular novamente
                 cameraProvider.unbindAll()
 
-                // Bind use cases to camera
-                cameraProvider.bindToLifecycle(
+                // Vincule os casos de uso à câmera
+                camera = cameraProvider.bindToLifecycle(
                     this, cameraSelector, preview, imageCapture
                 )
 
             } catch (exc: Exception) {
-                Log.e(TAG, "Use case binding failed", exc)
+                Log.e(TAG, "Falha na vinculação dos casos de uso", exc)
             }
 
         }, ContextCompat.getMainExecutor(this))
@@ -93,10 +154,9 @@ class MainActivity : AppCompatActivity() {
         ContextCompat.checkSelfPermission(baseContext, it) == PackageManager.PERMISSION_GRANTED
     }
 
-    // checks the camera permission
+    // Verifica a permissão da câmera
     override fun onRequestPermissionsResult(
-        requestCode: Int, permissions: Array<String>, grantResults:
-        IntArray
+        requestCode: Int, permissions: Array<String>, grantResults: IntArray
     ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
 
@@ -104,7 +164,7 @@ class MainActivity : AppCompatActivity() {
             if (allPermissionsGranted()) {
                 startCamera()
             } else {
-                Toast.makeText(this, "Permissions not granted by the user.", Toast.LENGTH_SHORT)
+                Toast.makeText(this, "Permissões não concedidas pelo usuário.", Toast.LENGTH_SHORT)
                     .show()
                 finish()
             }
